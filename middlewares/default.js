aafaa94249c96282ef7c6ab6f3d9b5aa9f8d6694
Ivan Sergeev <ahrejlok2007@ya.ru>
2020-02-08 17:06:49 +0500

    9 commit

--- middlewares/default.js
+++ middlewares/default.js
@@ -1,6 +1,5 @@
 const answers = require('../answers/answers');
+const ErrorsList = require('../errors/errorsList');
 
-const defaultUrl = (req, res) => {
-  res.status(404).send({ message: answers.urlNotFound });
-};
+const defaultUrl = (req, res, next) => next(new ErrorsList.BadRequest(answers.urlNotFound));
 module.exports = defaultUrl;
