996c2e6150461b1db6a2d357a2e191166498be94
IvanSergeev59 <55297515+IvanSergeev59@users.noreply.github.com>
2020-02-08 17:11:34 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 
-const helmet = require('helmet');
 
-
-router.use(helmet());
 const auth = require('../middlewares/auth.js');
 const defaultUrl = require('../middlewares/default');
 router.use('/signin', require('../routes/login'));
